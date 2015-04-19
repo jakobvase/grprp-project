16649655fb70f4407d05a4b93c6cf171376f176b
Jakob Vase Frandsen <jakobvase@gmail.com>
2015-04-19 21:01:46 +0200

    Added TriangleTess files to gitignore.

---
+++ TriangleTess/bin/CMakeFiles/Geodesic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Geodesic.dir/Geodesic.c.o"
+  "Geodesic.pdb"
+  "Geodesic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Geodesic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
