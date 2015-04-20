d877a0b096ed137ccefb6db30af3fd5860b55c3f
Jannek <jannek.ek@gmail.com>
2015-04-20 23:18:04 +0200

    modern shaders not working

--- vertex.glsl
+++ vertex.glsl
@@ -1,12 +1,24 @@
-varying vec3 light;
+#version 420
+
+in vec4 MCvertex;
+in vec3 MCnormal;
+
+uniform mat4 MVMatrix;
+uniform mat4 MVPMatrix;
+uniform mat3 NormalMatrix;
+
+uniform vec3 LightPosition;
+
+out vec3 light;
+out vec3 MCposition;
 
 void main()
 {
-    gl_Position = gl_ModelViewProjectionMatrix*gl_Vertex;
+    MCposition = (MVPMatrix*MCvertex).xyz;
 
-    vec3 normal = gl_NormalMatrix*gl_Normal;
-    vec3 to_light = (gl_LightSource[0].position - (gl_ModelViewMatrix*gl_Vertex)).xyz;
+    vec3 normal = NormalMatrix*MCnormal;
+    vec3 to_light = LightPosition - (MVMatrix*MCvertex).xyz;
     vec3 n_to_light = normalize(to_light);
-    float intense = clamp(dot(normal, n_to_light), 0.0, 1.0);
-    light = gl_LightSource[0].diffuse.xyz * intense * 0.9 + 0.1;
+    float intense = clamp(dot(normal, n_to_light.xyz), 0.0, 1.0);
+    light = vec3(intense * 0.9 + 0.1);
 }
